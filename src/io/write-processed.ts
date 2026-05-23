86d9d9497fc2920d4af0009b0c1d413ba551440c
BrightNighter <129858054+bright9th@users.noreply.github.com>
2026-05-23 20:15:28 +0700

    total bytes info on process

--- src/io/write-processed.ts
+++ src/io/write-processed.ts
@@ -1,3 +1,4 @@
+import { byteSize } from "../utils/byte";
 import { writeJson } from "./write-json";
 import { writeSnapshot } from "./write-snapshots";
 
@@ -5,10 +6,12 @@ export async function writeProcessedSISO(name: String, data: unknown) {
   const path = `data/processed/SISO/${name}/current.json`;
   await writeJson(path, data);
   await writeSnapshot(path);
+  console.log(`- ${byteSize(data)} bytes written.`);
 }
 
 export async function writeProcessed(name: String, data: unknown) {
   const path = `data/processed/MISO/${name}/current.json`;
   await writeJson(path, data);
   await writeSnapshot(path);
+  console.log(`- ${byteSize(data)} bytes written.`);
 }
